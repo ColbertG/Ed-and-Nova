bf3a29ab83e2c6c8935387c50360b8ce3d6c4b99
ColbertG <colbertg20@gmail.com>
2026-03-04 10:57:39 -0600

    mis back ground

--- Assets/Scripts/ControllerBackGround.cs
+++ Assets/Scripts/ControllerBackGround.cs
@@ -5,30 +5,43 @@ using UnityEngine;
 public class ControllerBackGround : MonoBehaviour
 {
     [SerializeField]
-    GameObject cam;
+    GameObject Cam;
     [SerializeField]
-    float parallaxEffect;
+    float ParallaxEffect;
     float length;
-    float startPos;
-
+    float startPosX;
+    float height;
+    float startPosY;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position.x;
+        startPosX = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        //startPosY = transform.position.y;
+        //height = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float tempX = (Cam.transform.position.x * (1 - ParallaxEffect));
+        float distX = (Cam.transform.position.x * ParallaxEffect);
 
-        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        //float tempY = (Cam.transform.position.y * (1 - ParallaxEffect));
+        //float distY = (Cam.transform.position.y * ParallaxEffect);
 
-        if (temp > startPos + length) startPos += length;
-        else if (temp < startPos - length) startPos -= length;
+        transform.position = new Vector3(startPosX + tempX, transform.position.y, transform.position.z);
+
+        if (distX > startPosX + length) startPosX += length;
+        else if (distX < startPosX - length) startPosX -= length;
+
+
+        //transform.position = new Vector3(transform.position.x, startPosY + distY, transform.position.z);
+
+        //if (tempY > startPosY + height) startPosY += height;
+        //else if (tempY < startPosY - height) startPosY -= height;
 
     }
 }
